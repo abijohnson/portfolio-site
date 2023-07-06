fe62b3876e3506f913caaeb0adb49b684bedf2a4
Abi Johnson <abijohnson98@gmail.com>
2023-07-06 16:24:06 -0500

    Portfolio site 1 aj implement design (#2)
    * PORTFOLIO-1 Drop files from gitignore
    
    * PORTFOLIO-SITE-1 Design implemented
    
    ---------
    
    Co-authored-by: Abi Johnson <abi@getveedback.com>

---
+++ src/components/homepage/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="bg-white px-6 py-24 sm:py-32 lg:px-8" id="about">
+        <div class="mx-auto max-w-4xl text-center">
+            <h2 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">About me</h2>
+            <p class="mt-6 text-lg leading-8 text-gray-600">
+                I am a 2020 graduate of Western Oregon University (B.S in Computer Science). I currently live in Oklahoma, and enjoy remote work. When I’m not working, I am probably reading, taking care of my plants, or spending time with my sweet fiance.  Tech has always been a love of mine, and so has design. Hence why I love front-end development. Being both a UX/UI designer and developer, I design with the devs in mind and I dev with the designers in mind. I enjoy what I do, and enjoy working with people who do too!
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "About"
+};
+</script>
+
